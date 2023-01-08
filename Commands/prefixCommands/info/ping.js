3d6ea7d29184c5a974947dac5e2ead441a64ad03
MrAnri29 <mr.anri2007@gmail.com>
2023-01-08 22:50:07 +0400

    Translated commands

--- Commands/prefixCommands/info/ping.js
+++ Commands/prefixCommands/info/ping.js
@@ -2,7 +2,7 @@ const { Message, Client } = require("discord.js");
 
 module.exports = {
     name: "ping",
-    description: "ტესტ ბრძანება",
+    description: "replies with pong!",
     aliases: ["pi", "p"],
     /**
      * @param {Message} message
